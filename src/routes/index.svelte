b520ce602f76acc3131d604744409a9bc02a89f6
iamceege <hello@calebjacob.com>
2022-01-10 21:38:30 -0700

    Tweaking colors

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -9,7 +9,7 @@
 <div class="layout layout--vertical-align">
 	<div class="container">
 		<section class="section center">
-			<span class="icon icon--block icon--large fa-code color-text-2" />
+			<span class="icon icon--block icon--large fa-code color-primary" />
 			<h1 class="title title--1 uppercase">Time to get Svelte-y.</h1>
 			<h2 class="title title--2 color-text-2">Let's have some fun & make something cool.</h2>
 		</section>
